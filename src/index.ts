f404b5593b7a60d6b1c5e14110f2766f90536a7b
Umesh Joshi <uj19343@gmail.com>
2025-07-12 00:58:34 +0545

    file upload is made

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,10 @@ import postPhoto from "./routes/photo.routes";
 import { connectDatabase } from "./config/databaseConnect.config";
 import userSection from "./routes/user.routes";
 import cors from "cors";
-
+import multer from "multer";
+import { Request, Response } from "express";
+import fs from "fs";
+import path from "path";
 const app = express();
 const PORT = process.env.PORT;
 const DB_URI = process.env.DB_URI || "";
@@ -22,6 +25,32 @@ app.use(express.json());
 
 app.use("/api/photo", postPhoto);
 app.use("/api/user", userSection);
+const uploadDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
+// for upload data in cludinary
+// const upload = multer({ dest: "uploads/" });
+const storage = multer.diskStorage({
+  destination: function (req: Request, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req: Request, file, cb) {
+    return cb(null, `${Date.now()} - ${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post(
+  "/upload",
+  upload.single("profileImage"),
+  (req: Request, res: Response) => {
+    console.log(req.body);
+    console.log(req.file);
+    res.redirect("/");
+  }
+);
 
 app.listen(PORT, () => {
   console.log("server started");
